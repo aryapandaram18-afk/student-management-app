e26664632536da793ca8f8fa731bca59e367554f
fe <fe@siesgst.local>
2026-07-17 16:27:10 +0530

    Added student counter

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ function addStudent() {
         document.getElementById("studentName").value = "";
         li.innerHTML =
             name + ' <button onclick="this.parentElement.remove()">Delete</button>';
+        document.getElementById("count").innerText =
+            document.getElementById("studentList").children.length;
 
     }
 }
